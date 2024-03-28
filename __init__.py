36117cfad7f99d702cd256db5e0e2a6be9e2b12b
BlakeOne <30273164+BlakeOne@users.noreply.github.com>
2024-03-28 13:13:58 -0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .node import IPAdapterLegacyNoise
+
+NODE_CLASS_MAPPINGS = {"IPAdapterLegacyNoise": IPAdapterLegacyNoise}
+NODE_DISPLAY_NAME_MAPPINGS = {"IPAdapterLegacyNoise" : "IPAdapterLegacyNoise"}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
